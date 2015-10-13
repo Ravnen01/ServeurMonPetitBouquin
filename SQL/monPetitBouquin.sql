e51984175756bcd0762b22287f4a19f263671297
iem <etienne.lacroix@etu.univ-lyon1.fr>
2015-10-13 11:57:07 +0200

    Modification du types de l'ISBN des livres(INT->BIGINT) et ajout de données dans AUTHOR, BOOK et BOOK_AUTHOR

--- SQL/monPetitBouquin.sql
+++ SQL/monPetitBouquin.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Mar 13 Octobre 2015 à 11:29
+-- Généré le :  Mar 13 Octobre 2015 à 11:55
 -- Version du serveur :  5.6.20
 -- Version de PHP :  5.5.15
 
@@ -30,7 +30,14 @@ CREATE TABLE IF NOT EXISTS `AUTHOR` (
 `Id` int(11) NOT NULL,
   `Name` varchar(50) NOT NULL,
   `Firstname` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Contenu de la table `AUTHOR`
+--
+
+INSERT INTO `AUTHOR` (`Id`, `Name`, `Firstname`) VALUES
+(1, 'Tolkien', 'J.R.R');
 
 -- --------------------------------------------------------
 
@@ -39,9 +46,18 @@ CREATE TABLE IF NOT EXISTS `AUTHOR` (
 --
 
 CREATE TABLE IF NOT EXISTS `BOOK` (
-  `ISBN` int(13) NOT NULL,
+  `ISBN` bigint(20) NOT NULL,
   `Title` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `BOOK`
+--
+
+INSERT INTO `BOOK` (`ISBN`, `Title`) VALUES
+(978026110358, 'The Lord of the Rings, The two towers'),
+(9780007123827, 'The Lord of the Rings, The Fellowship of the Ring'),
+(9780261103597, 'The Lord of the Rings, The return of the King');
 
 -- --------------------------------------------------------
 
@@ -50,9 +66,18 @@ CREATE TABLE IF NOT EXISTS `BOOK` (
 --
 
 CREATE TABLE IF NOT EXISTS `BOOK_AUTHOR` (
-  `IdBook` int(11) NOT NULL,
+  `IdBook` bigint(20) NOT NULL,
   `IdAuthor` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `BOOK_AUTHOR`
+--
+
+INSERT INTO `BOOK_AUTHOR` (`IdBook`, `IdAuthor`) VALUES
+(978026110358, 1),
+(9780007123827, 1),
+(9780261103597, 1);
 
 -- --------------------------------------------------------
 
@@ -61,7 +86,7 @@ CREATE TABLE IF NOT EXISTS `BOOK_AUTHOR` (
 --
 
 CREATE TABLE IF NOT EXISTS `CRITICISM` (
-  `IdBook` int(13) NOT NULL,
+  `IdBook` bigint(20) NOT NULL,
   `IdUser` int(13) NOT NULL,
   `Rate` int(1) DEFAULT NULL,
   `Comment` varchar(255) NOT NULL
@@ -103,7 +128,7 @@ ALTER TABLE `CRITICISM`
 -- AUTO_INCREMENT pour la table `AUTHOR`
 --
 ALTER TABLE `AUTHOR`
-MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- Contraintes pour les tables exportées
 --
