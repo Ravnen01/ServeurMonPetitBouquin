4ead87d34d4cf89c31b435b0acadaa4cbbaef2e1
iem <etienne.lacroix@etu.univ-lyon1.fr>
2015-11-02 09:00:48 +0100

    Add SQL requests in php index file

--- api/index.php
+++ api/index.php
@@ -36,6 +36,110 @@ if ($mysqli->connect_errno) {
 
 });
 
+$app->get('/author', function () {
+	$mysqli = new mysqli(DB_SERVER, DB_USER, DB_PASSWORD, DB);
+if ($mysqli->connect_errno) {
+    echo "Echec lors de la connexion à MySQL : (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+    $sql = $mysqli->query("SELECT Name, Firstname, COUNT(IdBook) FROM AUTHOR, BOOK_AUTHOR WHERE Id = IdAuthor GROUP BY Name, Firstname");
+    //var_dump($sql);
+    //die;
+
+            //$result = array();
+            while($rlt = $sql->fetch_array(MYSQLI_ASSOC))
+            {
+            	$result[] = $rlt;
+            }
+            // If success everythig is good send header as "OK" and return list of users in JSON format
+        $return = json_encode($result);
+        var_dump($return);
+        die;
+
+});
+
+$app->get('/critism/:bookId', function () {
+	$mysqli = new mysqli(DB_SERVER, DB_USER, DB_PASSWORD, DB);
+if ($mysqli->connect_errno) {
+    echo "Echec lors de la connexion à MySQL : (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+    $sql = $mysqli->query("SELECT IdBook, Rate, Comment FROM CRITICISM C WHERE IdBook = $bookId");
+    //var_dump($sql);
+    //die;
+
+            //$result = array();
+            while($rlt = $sql->fetch_array(MYSQLI_ASSOC))
+            {
+            	$result[] = $rlt;
+            }
+            // If success everythig is good send header as "OK" and return list of users in JSON format
+        $return = json_encode($result);
+        var_dump($return);
+        die;
+
+});
+
+$app->get('/critism/:bookId', function () {
+	$mysqli = new mysqli(DB_SERVER, DB_USER, DB_PASSWORD, DB);
+if ($mysqli->connect_errno) {
+    echo "Echec lors de la connexion à MySQL : (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+    $sql = $mysqli->query("SELECT IdBook, Rate, Comment FROM CRITICISM C WHERE IdBook = $bookId");
+    //var_dump($sql);
+    //die;
+
+            //$result = array();
+            while($rlt = $sql->fetch_array(MYSQLI_ASSOC))
+            {
+            	$result[] = $rlt;
+            }
+            // If success everythig is good send header as "OK" and return list of users in JSON format
+        $return = json_encode($result);
+        var_dump($return);
+        die;
+
+});
+
+$app->get('/search/ISBN=:bookId&Author=:authorName&Title=:title', function () {
+	$mysqli = new mysqli(DB_SERVER, DB_USER, DB_PASSWORD, DB);
+    if ($mysqli->connect_errno) {
+        echo "Echec lors de la connexion à MySQL : (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    }
+    if($bookId!="" && $authorId=="" && $title=="")
+    {
+        $sql = $mysqli->query("SELECT ISBN, Title, Name, Firstname FROM BOOK, AUTHOR, BOOK_AUTHOR WHERE ISBN = IdBook AND IdAuthor = Id AND (CAST(ISBN AS CHAR) LIKE $bookId)");
+        while($rlt = $sql->fetch_array(MYSQLI_ASSOC))
+        {
+            $result[] = $rlt;
+        }
+    }
+    else if($bookId=="" && $authorName!="" && $title=="")
+    {
+        $sql = $mysqli->query("SELECT ISBN, Title, Name, Firstname FROM BOOK, AUTHOR, BOOK_AUTHOR WHERE ISBN = IdBook AND IdAuthor = Id AND (Name LIKE $authorName OR Firstname LIKE $authorName)");
+        while($rlt = $sql->fetch_array(MYSQLI_ASSOC))
+        {
+            $result[] = $rlt;
+        }
+    }
+    else if($bookId=="" && $authorName="" && $title!="")
+    {
+        $sql = $mysqli->query("SELECT ISBN, Title, Name, Firstname FROM BOOK, AUTHOR, BOOK_AUTHOR WHERE ISBN = IdBook AND IdAuthor = Id AND (Title LIKE $title)");
+        while($rlt = $sql->fetch_array(MYSQLI_ASSOC))
+        {
+            $result[] = $rlt;
+        }
+    }
+    else if($bookId=="" && $authorName="" && $title=="")
+    {
+        echo "You didn't entered a research";
+    }
+            //$result = array();
+    // If success everythig is good send header as "OK" and return list of users in JSON format
+    $return = json_encode($result);
+    var_dump($return);
+    die;
+
+});
+
 
 $app->run();
 
